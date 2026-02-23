424f1a94cc42880907e86b14d57540ac5faf1b30
muwatta <abdullahmusliudeen@gmail.com>
2026-02-23 12:46:07 +0100

    first commit

--- scripts/start_detection.py
+++ scripts/start_detection.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add project root to Python path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(ROOT_DIR)
+
 import cv2
 from backend.classifier import detect_pest
 from backend.storage import save_snapshot
